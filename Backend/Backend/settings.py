711813af4e725c53e913bd11480a5e3dd0b93bf2
unknown <rc.goutam.choudhary@gmail.com>
2025-11-04 19:07:40 +0530

    revert /media/

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -205,8 +205,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# MEDIA_URL = "/media/"
-MEDIA_URL = "media/" #temparary changes
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
